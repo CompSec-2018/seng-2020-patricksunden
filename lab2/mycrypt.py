9c863acd89296416f9a0d46f27d6d80f05342f12
Patrick Sunden <37216706+patricksunden@users.noreply.github.com>
2020-12-29 16:12:54 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,4 +1,5 @@
 import codecs
+import string
 
 def encode(s):
     if not isinstance(s,str):
@@ -8,17 +9,45 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    s = s.ljust(1000, 'k') #Pad the string
+
     for c in s:
-        if c.isalpha():
+        if c == '+' or c == '-' or c == 'ä' or c == 'å' or c == 'ö':#if the input is one of these, raise ValueError
+            raise ValueError
+
+        if c in digitmapping:
+            crypted+=digitmapping[c]
+        elif c.isalpha():
             if c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
-        elif c in digitmapping:
-          crypted+=digitmapping[c]
+        
+        elif c == " ":
+            c.replace(" ", "")
+        else:
+            raise ValueError
 
-    return crypted
+    return crypted[:origlen] #returning only the wanted length
 
 def decode(s):
-    return s
+    if not isinstance(s,str):
+        raise TypeError
+    origlen = len(s)
+    crypted = ""
+    decrypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    if len(s) > 1000:
+        raise ValueError
+    for c in s:
+        if c.isalpha():
+            if c.isupper():
+                c=c.lower()
+            decrypted += codecs.decode(c, 'rot13')
+        elif c in digitmapping:
+            decrypted += digitmapping[c]
+        else:
+            raise ValueError
+
+    return decrypted
 

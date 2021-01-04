a95928a3b582d1222924268560a1fbf5c6d27f5d
Patrick Sunden <37216706+patricksunden@users.noreply.github.com>
2021-01-04 12:55:52 +0200

    Update test_mycrypt.py

--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -5,9 +5,7 @@
 Unit tests for mycrypt function. Basically ROT13, but also
 capitalize or uncapitalize, and for numbers, replace with shifted
 versions.
-
 tr 'A-Za-z0-9=!"#€%&/()' 'n-za-mN-ZA-M=!"#€%&/()0-9'
-
 If characters outside allowed ones are used as input, raise ValueError.
 '''
 
@@ -35,20 +33,19 @@ def test_encode_decode(test_input):
     assert(mycrypt.decode(mycrypt.encode(test_input))) == test_input
 
 
-@pytest.mark.parametrize("invalid_input", ['+','åäö'])
+@pytest.mark.parametrize("invalid_input", ['+','åäö', 'a'*1001])
 def test_invalid_char(invalid_input):
     '''Invalid characters should result in ValueError'''
     with pytest.raises(ValueError):
         mycrypt.encode(invalid_input)
 
 
-@pytest.mark.parametrize("invalid_input", [])
+@pytest.mark.parametrize("invalid_input", [4])
 def test_invalid_types(invalid_input):
     '''Invalid parameter types should raise TypeError'''
     with pytest.raises(TypeError):
         mycrypt.encode(invalid_input)
-    with pytest.raises(SystemExit):
-        mycrypt.encode(invalid_input)
+    
 
 
     
@@ -56,10 +53,8 @@ def test_invalid_types(invalid_input):
 def test_timing():
     '''Test whether encoding runs in approximately constant time, repetitions
     kept low to make test fast, use smallest measured time.
-
     Note: Tests like this need quite a bit of thought when used as a unit test,
     they are non-deterministic and might fail randomly.
-
     Hint: pad your string to max length and only return wanted length
     '''
     timing1 = min(timeit.repeat('mycrypt.encode("a")',
@@ -67,4 +62,3 @@ def test_timing():
     timing2 = min(timeit.repeat('mycrypt.encode("a"*1000)',
                                 'import mycrypt', repeat=3, number=30))
     assert 0.95 * timing2 < timing1 < 1.05 * timing2
-
